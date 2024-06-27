a9e4f16fef7346aa05fef6c5c318e85158dbc50c
harsimranjit-singh7 <92724441+dhruvpuri-goswami@users.noreply.github.com>
2024-06-27 16:43:58 +0530

    minor changes

--- pipedetection/settings.py
+++ pipedetection/settings.py
@@ -125,8 +125,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import os
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
