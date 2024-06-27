9287a38291ebc7f1558843d2539c42a01806abfb
harsimranjit-singh7 <92724441+dhruvpuri-goswami@users.noreply.github.com>
2024-06-27 16:42:34 +0530

    changes done

--- pipedetection/settings.py
+++ pipedetection/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$kk(8x=mw&69r$9$$bwazv^9v5&#u)tsfon$#)bbxgsrhp(se=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True  # Ensure this is set appropriately based on your environment.
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
