4847fd52a2cc1fc79a820493637e7741b456c978
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-07-10 18:59:53 +0530

    Add: Deployment Files

--- pipedetection/settings.py
+++ pipedetection/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv("REQUEST_HOST1"), os.getenv("REQUEST_HOST2")]
+ALLOWED_HOSTS = [os.getenv("REQUEST_HOST1"), os.getenv("REQUEST_HOST2"), '.vercel.app']
 
 
 # Application definition
